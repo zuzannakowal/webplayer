6d8711f23804b0d4114ad47662fb1a1d9a0eea1c
zuzannakowal <zuzanna@kowal.org.pl>
2021-05-22 22:22:33 +0200

    player

--- CLIENT/src/components/Player.vue
+++ CLIENT/src/components/Player.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="player">
-    player
+    <song-name
+      v-for="song in songs"
+      v-bind:key="song.id"
+      :song="song"
+    ></song-name>
+    <play></play>
+    <time></time>
+
   </div>
 </template>
+<script>
+  import SongName from "./SongName.vue";
+  import Play from "./Play.vue";
+  import Time from "./Time.vue"
+  export default {
+    components: {SongName, Play, Time},
+    props: ['songs'],
+  }
+</script>
 <style scoped>
   .player{
     height: 100px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
   }
 </style>
